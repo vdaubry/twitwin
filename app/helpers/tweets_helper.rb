3edecb5ca1f924c3bb6da558cfcd1be244b43e52
Vincent Daubry <vdaubry@gmail.com>
2015-09-12 10:49:19 +0200

    set bold text in css

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,7 +4,7 @@ module TweetsHelper
       if played.include? tweet
         link_to raw("<b>#{t('play_button.played')}</b>"), "#", class: "btn btn-default btn-play"
       else
-        link_to(t('play_button.title'), tweet_participation_index_path(tweet.id), remote: true, method: :post, class: "btn btn-success btn-play activated")
+        link_to(t('play_button.title'), tweet_participation_index_path(tweet.id), remote: true, method: :post, class: "btn btn-success btn-play activated font-bold")
       end
     else
       link_to raw("<b>#{t('play_button.connect')}</b>"), "auth/twitter", :onclick => "$(\"#modal-window\").modal('show')", class: "btn btn-success btn-play"
